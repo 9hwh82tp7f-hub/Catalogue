d972fb5f278c9976c959808c5cae590dba4f40c7
9hwh82tp7f-hub <9hwh82tp7f@privaterelay.appleid.com>
2026-08-18 01:53:31 +0200

    Delete assets directory

--- assets/admin/v9/app.js
+++
@@ -1,4 +0,0 @@
-import {createSnapshot,listSnapshots,getSnapshot,removeSnapshot,clearSnapshots,compareSnapshots,fingerprint} from './state.js';
-import {validateCatalogue} from './schema.js';
-import {GitHubClient} from './github.js';
-window.CatalogueV9={createSnapshot,listSnapshots,getSnapshot,removeSnapshot,clearSnapshots,compareSnapshots,fingerprint,validateCatalogue,GitHubClient,version:'9.0'};
