f457c681312358e46cdddafd263e0d88fbec36fc
9hwh82tp7f-hub <9hwh82tp7f@privaterelay.appleid.com>
2026-08-19 09:26:53 +0200

    Delete assets directory

--- assets/admin/app.js
+++
@@ -1,3 +0,0 @@
-import {createSnapshot,listSnapshots,removeSnapshot,clearSnapshots,compareSnapshots,fingerprint} from './state.js';
-import {validateCatalogue} from './schema.js';
-window.CatalogueAdmin={createSnapshot,listSnapshots,getSnapshot,removeSnapshot,clearSnapshots,compareSnapshots,fingerprint,validateCatalogue,version:'13.0'};
