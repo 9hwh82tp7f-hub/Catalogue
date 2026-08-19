8b73119febc01c7443c2ad059b304cefd6b0742c
9hwh82tp7f-hub <9hwh82tp7f@privaterelay.appleid.com>
2026-08-19 12:18:34 +0200

    Add files via upload

---
+++ assets/admin/app.js
@@ -0,0 +1,13 @@
+import {createSnapshot,listSnapshots,removeSnapshot,clearSnapshots,compareSnapshots,fingerprint} from './state.js';
+import {validateCatalogue} from './schema.js';
+
+window.CatalogueAdmin={
+  createSnapshot,
+  listSnapshots,
+  removeSnapshot,
+  clearSnapshots,
+  compareSnapshots,
+  fingerprint,
+  validateCatalogue,
+  version:'13.0'
+};
